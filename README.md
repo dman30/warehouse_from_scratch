30a187cbdf3f0f0f7c0395c3fdd5565ab4915dd6
Damoa Obiri Alcorta <damoa.obiri@kaeuferportal.de>
2012-10-22 10:45:48 +0200

    initial commit + README improvement

---
+++ README.md
@@ -0,0 +1,7 @@
+# Warehouse from scratch
+
+This is a management system for the inventory management.
+Administrativ users are allowed to create articles and assign them to
+employees.
+Other users will only be allowed to see employees with their respective
+assigned articles.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarehouseFromScratch::Application.config.secret_token = '9337b1017783a36decfce057990652fe225793ba58d180aa605ac01b6fdea7d36068b96e4508df475f59b36f208d255d4cbd482760df75d47e8a146338900f80'
